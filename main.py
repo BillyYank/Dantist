e7c84a08102de9fd6aa073e17e3bea870d00f489
Georgy Borisenko <gosha-bor@yandex.ru>
2017-04-04 01:52:38 +0300

    multiface

--- main.py
+++ main.py
@@ -24,60 +24,61 @@ print 'detecting face'
 result = CF.face.detect(FileData, face_id = False, landmarks = True)              # запрос
 print result
 
-# создание словаря rot
-rot = {'mouthRight':result[0]['faceLandmarks']['mouthRight'],       # правый край рта
-'mouthLeft':result[0]['faceLandmarks']['mouthLeft'],           # левый край рта
-'upperLipBottom':result[0]['faceLandmarks']['upperLipBottom'],         # верхний кончик верхней губы, можно нижний сделать
-'underLipBottom':result[0]['faceLandmarks']['underLipBottom']}      # нижний кончик нижней губы
-
-# Округление
-rot['mouthRight']['x']=round(rot['mouthRight']['x'])
-rot['mouthRight']['y']=round(rot['mouthRight']['y'])
-rot['upperLipBottom']['x']=round(rot['upperLipBottom']['x'])
-rot['upperLipBottom']['y']=round(rot['upperLipBottom']['y'])
-rot['mouthLeft']['x'] = round(rot['mouthLeft']['x'])
-rot['mouthLeft']['y'] = round(rot['mouthLeft']['y'])
-rot['underLipBottom']['x'] = round(rot['underLipBottom']['x'])
-rot['underLipBottom']['y'] = round(rot['underLipBottom']['y'])
-
-print 'creating mask'
-# создание полигона
-right, top, left, bottom = map(np.array, [
-    (rot['mouthRight']['x'],rot['mouthRight']['y']),
-    (rot['upperLipBottom']['x'],rot['upperLipBottom']['y']),
-    (rot['mouthLeft']['x'],rot['mouthLeft']['y']),
-    (rot['underLipBottom']['x'],rot['underLipBottom']['y'])])
-
-vertex = [left, top, right, 
-    (right[0]*0.5 + bottom[0] * 0.5, right[1] * 0.3 + bottom[1] * 0.7),
-    bottom,
-    (left[0]*0.5 + bottom[0] * 0.5, left[1] * 0.3 + bottom[1] * 0.7),
-    ]
-
-poly = shapely.geometry.Polygon(vertex)
-
-#Определение размеров изображения
 image = PIL.Image.open(img_path) # it will be used later
 print 'Image size:', image.size
 width, height = image.size
 size = (height, width)
 
-min_x = int(min(zip(*vertex)[0]))
-max_x = int(max(zip(*vertex)[0]))
-min_y = int(min(zip(*vertex)[1]))
-max_y = int(max(zip(*vertex)[1]))
-print 'min_x', min_x
-print 'max_x', max_x
-print 'min_y', min_y
-print 'max_y', max_y
-
-
-# создание маски
 mask_mouth = np.zeros(size).astype(bool)
-for i in range(min_y, max_y + 1):
-    for j in range(min_x, max_x + 1):
-        point = shapely.geometry.Point(j, i)
-        mask_mouth[i, j] = point.intersects(poly)
+
+for index in range(len(result)):
+    print 'face {} of {}'.format(index + 1, len(result)) 
+# создание словаря rot
+    rot = {'mouthRight':result[index]['faceLandmarks']['mouthRight'],       # правый край рта
+    'mouthLeft':result[index]['faceLandmarks']['mouthLeft'],           # левый край рта
+    'upperLipBottom':result[index]['faceLandmarks']['upperLipBottom'],         # верхний кончик верхней губы, можно нижний сделать
+    'underLipBottom':result[index]['faceLandmarks']['underLipBottom']}      # нижний кончик нижней губы
+
+# Округление
+    rot['mouthRight']['x']=round(rot['mouthRight']['x'])
+    rot['mouthRight']['y']=round(rot['mouthRight']['y'])
+    rot['upperLipBottom']['x']=round(rot['upperLipBottom']['x'])
+    rot['upperLipBottom']['y']=round(rot['upperLipBottom']['y'])
+    rot['mouthLeft']['x'] = round(rot['mouthLeft']['x'])
+    rot['mouthLeft']['y'] = round(rot['mouthLeft']['y'])
+    rot['underLipBottom']['x'] = round(rot['underLipBottom']['x'])
+    rot['underLipBottom']['y'] = round(rot['underLipBottom']['y'])
+
+    print 'creating mask'
+# создание полигона
+    right, top, left, bottom = map(np.array, [
+        (rot['mouthRight']['x'],rot['mouthRight']['y']),
+        (rot['upperLipBottom']['x'],rot['upperLipBottom']['y']),
+        (rot['mouthLeft']['x'],rot['mouthLeft']['y']),
+        (rot['underLipBottom']['x'],rot['underLipBottom']['y'])])
+
+    vertex = [left, top, right, 
+        (right[0]*0.5 + bottom[0] * 0.5, right[1] * 0.3 + bottom[1] * 0.7),
+        bottom,
+        (left[0]*0.5 + bottom[0] * 0.5, left[1] * 0.3 + bottom[1] * 0.7),
+        ]
+
+    poly = shapely.geometry.Polygon(vertex)
+
+
+    min_x = int(min(zip(*vertex)[0]))
+    max_x = int(max(zip(*vertex)[0]))
+    min_y = int(min(zip(*vertex)[1]))
+    max_y = int(max(zip(*vertex)[1]))
+    print 'min_x', min_x
+    print 'max_x', max_x
+    print 'min_y', min_y
+    print 'max_y', max_y
+
+    for i in range(min_y, max_y + 1):
+        for j in range(min_x, max_x + 1):
+            point = shapely.geometry.Point(j, i)
+            mask_mouth[i, j] = point.intersects(poly)
 
 #plt.imshow(mask_mouth, interpolation='nearest', cmap = 'bone')
 #plt.show()
